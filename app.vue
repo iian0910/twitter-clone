f4f3b0a2fa43fc8d3e04ae3952800c9555f52fb0
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2024-08-27 10:35:57 +0800

    加入 tailwindcss 並客製化顏色

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div :class="{'dark' : darkMode}">
+    <h1 class="text-red-500 dark:text-dim-500">Twitter Clone</h1>
   </div>
 </template>
+
+<script setup>
+const darkMode = ref(true)
+
+</script>
