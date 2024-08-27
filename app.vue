5829b0b0c2fe596e26dd15d79b6911cbeeee67c4
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2024-08-27 15:13:38 +0800

    製作左側側邊攔

--- app.vue
+++ app.vue
@@ -1,10 +1,36 @@
 <template>
   <div :class="{'dark' : darkMode}">
-    <h1 class="text-red-500 dark:text-dim-500">Twitter Clone</h1>
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="
+          grid
+          grid-cols-12
+          mx-auto
+          sm:px-6
+          lg:max-w-7xl
+          lg:px-8
+          lg:gap-5
+        ">
+          <!-- Left Sidebar -->
+            <div class="
+              hidden
+              md:block
+              xs-col-span-1
+              xl:col-span-2
+            ">
+              <div class="sticky top-0">
+                <SidebarLeft/>
+              </div>
+            </div>
+          <!-- Main -->
+          <!-- Right Sidebar -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-const darkMode = ref(true)
+const darkMode = ref(false)
 
 </script>
---
+++ components/Sidebar/Left/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <div
+      class="
+        p-2
+        my-2
+        hover:bg-blue-50
+        dark:hover:bg-white/20
+        hover:rounded-full
+        w-min
+      "
+      :class="defaultTransition"
+    >
+      <nuxt-link to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+    <div class="mt-2 space-y-3">
+      <SidebarLeftTab v-for="item in tabList" :key="item.name">
+        <template v-slot:icon>
+          <component :is="item.icon"/>
+        </template>
+        <template v-slot:name>
+          {{ item.name }}
+        </template>
+      </SidebarLeftTab>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { HomeIcon } from "@heroicons/vue/solid"
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  DocumentTextIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon
+} from "@heroicons/vue/outline"
+const { defaultTransition } = useTailwindConfig
+
+const tabList = ref([
+  { name: 'Home', icon: HomeIcon },
+  { name: 'Explore', icon: HashtagIcon },
+  { name: 'Notifications', icon: BellIcon },
+  { name: 'Messages', icon: InboxIcon },
+  { name: 'Bookmark', icon: BookmarkIcon },
+  { name: 'Lists', icon: DocumentTextIcon },
+  { name: 'Profile', icon: UserIcon },
+  { name: 'More', icon: DotsCircleHorizontalIcon }
+])
+</script>
